cccba75616a1e7daf79f41e4169eed5ba57127be
josueespinozacarlos16-dev <josueespinozacarlos16@gmail.com>
2026-09-18 07:25:45 -0500

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/vibra/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.vibra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    // ¡Esto es lo que faltaba! Usa la versión nueva de Android
+}
