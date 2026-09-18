23fc0dc191a38e371d5b661105c0aad78fe03b2d
josueespinozacarlos16-dev <josueespinozacarlos16@gmail.com>
2026-09-18 07:34:25 -0500

    Delete android directory

--- android/app/src/main/kotlin/com/example/vibra/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.vibra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-    // ¡Esto es lo que faltaba! Usa la versión nueva de Android
-}
